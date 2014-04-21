d9c067e45ed0eab535cf2f06d981a0c5832646b0
Matheus Borges Teixeira <matheus.bt@gmail.com>
2014-04-21 19:06:46 +0200

    Fixing Goals mapping.

--- src/main/java/org/arkanos/aos/api/servlets/Goals.java
+++ src/main/java/org/arkanos/aos/api/servlets/Goals.java
@@ -15,7 +15,7 @@ import org.arkanos.aos.api.data.Goal;
 /**
  * Servlet implementation class Goals
  */
-@WebServlet("goals")
+@WebServlet("/goals")
 public class Goals extends HttpServlet {
 	private static final long	serialVersionUID	= 1L;
 	
