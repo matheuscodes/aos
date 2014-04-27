b739b643858cb0991549a15d0d38e2f38e87ea57
Matheus Borges Teixeira <matheus.bt@gmail.com>
2014-04-27 20:58:27 +0200

    Finishing Goal API, data access and fixing issues.

--- src/main/java/org/arkanos/aos/api/data/Goal.java
+++ src/main/java/org/arkanos/aos/api/data/Goal.java
@@ -18,27 +18,85 @@
  */
 package org.arkanos.aos.api.data;
 
+import java.io.IOException;
+import java.io.Reader;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.util.Vector;
 
 import org.arkanos.aos.api.controllers.Database;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * @author arkanos
  * 
  */
 public class Goal {
-	static final String	TABLE_NAME				= "goal";
-	static final String	FIELD_ID				= "id";
-	static final String	FIELD_TITLE				= "title";
-	static final String	FIELD_TIME_PLANNED		= "time_planned";
-	static final String	FIELD_DESCRIPTION		= "description";
-	static final String	FIELD_USER_NAME			= "user_name";
-	static final String	EXTRA_COMPLETION		= "completion";
-	static final String	EXTRA_DEDICATION		= "dedication";
-	static final String	EXTRA_TOTAL_TIME_SPENT	= "total_time_spent";
+	static public final String	TABLE_NAME				= "goal";
+	static public final String	FIELD_ID				= "id";
+	static public final String	FIELD_TITLE				= "title";
+	static public final String	FIELD_TIME_PLANNED		= "time_planned";
+	static public final String	FIELD_DESCRIPTION		= "description";
+	static public final String	FIELD_USER_NAME			= "user_name";
+	static public final String	EXTRA_COMPLETION		= "completion";
+	static public final String	EXTRA_DEDICATION		= "dedication";
+	static public final String	EXTRA_TOTAL_TIME_SPENT	= "total_time_spent";
+	
+	static public int createGoal(String title, int time_planned, String description, String user_name) {
+		// TODO Auto-generated constructor stub
+		boolean insertion = Database.execute("INSERT INTO " + Goal.TABLE_NAME + " ("
+												+ Goal.FIELD_TITLE + ","
+												+ Goal.FIELD_TIME_PLANNED + ","
+												+ Goal.FIELD_DESCRIPTION + ","
+												+ Goal.FIELD_USER_NAME + ") "
+												+ "VALUES (\""
+												+ title + "\"," + time_planned + ",\""
+												+ description + "\",\"" + user_name + "\");");
+		if (insertion == true) {
+			try {
+				ResultSet rs = Database.query("SELECT MAX(" + Goal.FIELD_ID + ") AS created_id"
+												+ " FROM " + Goal.TABLE_NAME + " WHERE "
+												+ Goal.FIELD_TITLE + " = \"" + title + "\" AND "
+												+ Goal.FIELD_TIME_PLANNED + " = " + time_planned + " AND "
+												+ Goal.FIELD_DESCRIPTION + " = " + description + " AND "
+												+ Goal.FIELD_USER_NAME + " = \"" + user_name + "\";");
+				if (rs.next()) return rs.getInt("created_id");
+			}
+			catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @param id2
+	 * @return
+	 */
+	public static Goal getGoal(String user_name, int id) {
+		try {
+			ResultSet rs = Database.query("SELECT * FROM " + Goal.TABLE_NAME + " WHERE "
+											+ Goal.FIELD_USER_NAME + " = \"" + user_name + "\""
+											+ " AND " + Goal.FIELD_ID + " = " + id + ";");
+			if (rs.next()) {
+				Goal newone = new Goal(rs.getInt(Goal.FIELD_ID),
+										rs.getString(Goal.FIELD_TITLE),
+										rs.getInt(Goal.FIELD_TIME_PLANNED),
+										rs.getString(Goal.FIELD_DESCRIPTION),
+										user_name);
+				return newone;
+			}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	static public Vector<Goal> getUserGoals(String user_name) {
 		try {
@@ -49,7 +107,8 @@ public class Goal {
 				Goal newone = new Goal(rs.getInt(Goal.FIELD_ID),
 										rs.getString(Goal.FIELD_TITLE),
 										rs.getInt(Goal.FIELD_TIME_PLANNED),
-										rs.getString(Goal.FIELD_DESCRIPTION));
+										rs.getString(Goal.FIELD_DESCRIPTION),
+										user_name);
 				//TODO optimize?
 				ResultSet newrs = Database.query("SELECT AVG(help.progress) AS completion, SUM(help.spent) AS total_time_spent FROM ("
 													+ "SELECT IF(SUM((w." + Work.FIELD_RESULT + ")/(t." + Task.FIELD_TARGET + "-t." + Task.FIELD_INITIAL + ")) IS NULL, 0, SUM((w." + Work.FIELD_RESULT + ")/(t." + Task.FIELD_TARGET + "-t." + Task.FIELD_INITIAL + "))) AS progress, "
@@ -63,6 +122,7 @@ public class Goal {
 					newone.setCompletion(newrs.getFloat("completion"));
 					newone.setTotalTimeSpent(newrs.getInt("total_time_spent"));
 				}
+				
 				results.add(newone);
 				
 			}
@@ -92,18 +152,47 @@ public class Goal {
 		return false;
 	}
 	
-	private final int		id;
-	private final int		time_planned;
-	private final String	title;
+	static public Goal parseGoal(Reader from, String user_name) {
+		JSONParser jp = new JSONParser();
+		try {
+			//TODO send this shitty simple-JSON to fuck
+			JSONObject jo = (JSONObject) jp.parse(from);
+			String id = "" + jo.get(Goal.FIELD_ID);
+			String title = "" + jo.get(Goal.FIELD_TITLE);
+			String time_planned = "" + jo.get(Goal.FIELD_TIME_PLANNED);
+			String description = "" + jo.get(Goal.FIELD_DESCRIPTION);
+			Goal newone = new Goal(Integer.parseInt(id), title,
+									(int) (Float.parseFloat(time_planned) * 60), description, user_name);
+			newone.setCompletion(Float.parseFloat("" + jo.get(Task.EXTRA_COMPLETION)));
+			newone.setTotalTimeSpent((int) (Float.parseFloat("" + jo.get(Goal.EXTRA_TOTAL_TIME_SPENT)) * 60));
+			return newone;
+		}
+		catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
-	private final String	description;
+	private final int		id;
+	
+	private int				time_planned;
+	
+	private String			title;
+	private String			description;
+	private final String	user_name;
+	
 	/* Volatile data */
 	private float			completion			= 0;
-	
 	private int				total_time_spent	= 0;
 	
-	public Goal(int id, String title, int time_planned, String description) {
+	public Goal(int id, String title, int time_planned, String description, String user_name) {
 		this.id = id;
+		this.user_name = user_name;
 		this.time_planned = time_planned;
 		if (title != null) {
 			this.title = title;
@@ -119,8 +208,26 @@ public class Goal {
 		}
 	}
 	
+	public boolean delete() {
+		return Database.execute("DELETE FROM " + Goal.TABLE_NAME
+								+ " WHERE " + Goal.FIELD_ID + " = " + this.id
+								+ " AND " + Goal.FIELD_USER_NAME + " = \"" + this.user_name + "\";");
+	}
+	
 	private int getId() {
 		return this.id;
+	}
+	
+	public void replaceContent(Goal to) {
+		/* NEVER
+		 * this.id = to.id;
+		 * this.user_name = to.user_name;
+		 */
+		this.title = Database.sanitizeString(to.title);
+		this.description = Database.sanitizeString(to.description);
+		this.time_planned = to.time_planned;
+		this.completion = to.completion;
+		this.total_time_spent = to.total_time_spent;
 	}
 	
 	private void setCompletion(float c) {
@@ -148,5 +255,14 @@ public class Goal {
 		result += Goal.EXTRA_COMPLETION + "\":" + df.format(this.completion * 100.0f) + ",\"";
 		result += Goal.FIELD_DESCRIPTION + "\":\"" + this.description + "\"}";
 		return result;
+	}
+	
+	public boolean update() {
+		return Database.execute("UPDATE " + Goal.TABLE_NAME + " SET "
+								+ Goal.FIELD_TITLE + " = \"" + this.title + "\","
+								+ Goal.FIELD_TIME_PLANNED + " = " + this.time_planned + ","
+								+ Goal.FIELD_DESCRIPTION + " = \"" + this.description + "\""
+								+ " WHERE " + Goal.FIELD_ID + " = " + this.id
+								+ " AND " + Goal.FIELD_USER_NAME + " = \"" + this.user_name + "\";");
 	}
 }
--- src/main/java/org/arkanos/aos/api/servlets/Goals.java
+++ src/main/java/org/arkanos/aos/api/servlets/Goals.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.arkanos.aos.api.controllers.Database;
 import org.arkanos.aos.api.controllers.HTTP;
 import org.arkanos.aos.api.controllers.Security;
 import org.arkanos.aos.api.data.Goal;
@@ -15,7 +16,7 @@ import org.arkanos.aos.api.data.Goal;
 /**
  * Servlet implementation class Goals
  */
-@WebServlet("/goals")
+@WebServlet({ "/goals", "/goals/*" })
 public class Goals extends HttpServlet {
 	private static final long	serialVersionUID	= 1L;
 	
@@ -25,6 +26,57 @@ public class Goals extends HttpServlet {
 	public Goals() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Security.TokenInfo token = Security.authenticateToken(request);
+		if (token == null) {
+			response.sendError(403, "Token is not valid or not found.");
+			return;
+		}
+		HTTP.setUpDefaultHeaders(response);
+		//TODO move to default headers
+		response.setContentType("application/x-json");
+		
+		String URI = request.getRequestURI();
+		if (URI.endsWith("/goals") || URI.endsWith("/goals/")) {
+			response.setHeader("Allow", "GET, POST");
+			response.sendError(405, "DELETE is not supported without a specific resource.");
+			return;
+		}
+		String resource = URI.substring(URI.lastIndexOf("/") + 1);
+		if (URI.endsWith("goals/" + resource)) {
+			int id = Integer.parseInt(resource);
+			Goal result = Goal.getGoal(token.getUsername(), id);
+			if (result != null) {
+				if (Goal.isUserGoal(token.getUsername(), id)) {
+					if (result.delete()) {
+						response.setStatus(204);
+						return;
+					}
+					else {
+						response.sendError(500, "Could not delete the goal.");
+						return;
+					}
+				}
+				else {
+					response.sendError(403, "Goal does not belong to user.");
+					return;
+				}
+			}
+			else {
+				response.sendError(404, "Goal not found.");
+				return;
+			}
+		}
+		else {
+			response.sendError(400, "Goal not properly identified.");
+			return;
+		}
 	}
 	
 	/**
@@ -52,6 +104,110 @@ public class Goals extends HttpServlet {
 		goals += "],\"count\":" + count + "}";
 		response.setStatus(200);
 		response.getWriter().print(goals);
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Security.TokenInfo token = Security.authenticateToken(request);
+		if (token == null) {
+			response.sendError(403, "Token is not valid or not found.");
+			return;
+		}
+		HTTP.setUpDefaultHeaders(response);
+		
+		String URI = request.getRequestURI();
+		if (!(URI.endsWith("/goals") || !URI.endsWith("/goals/"))) {
+			response.setHeader("Allow", "GET, PUT, DELETE");
+			response.sendError(405, "POST is not supported for a specific resource.");
+			return;
+		}
+		else {
+			if (!URI.endsWith("/")) {
+				URI += "/";
+			}
+		}
+		
+		String title = Database.sanitizeString(request.getParameter(Goal.FIELD_TITLE));
+		int time_planned = Integer.parseInt(request.getParameter(Goal.FIELD_TIME_PLANNED));
+		String description = Database.sanitizeString(request.getParameter(Goal.FIELD_DESCRIPTION));
+		
+		int id = Goal.createGoal(title, time_planned, description, token.getUsername());
+		Goal created = Goal.getGoal(token.getUsername(), id);
+		if (created != null) {
+			response.setHeader("Location", URI + id);
+			response.getWriter().print(created);
+			response.setStatus(201);
+			return;
+		}
+		else {
+			response.sendError(500, "Task could not be created.");
+			return;
+		}
+	}
+	
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Security.TokenInfo token = Security.authenticateToken(request);
+		if (token == null) {
+			response.sendError(403, "Token is not valid or not found.");
+			return;
+		}
+		HTTP.setUpDefaultHeaders(response);
+		//TODO move to default headers
+		response.setContentType("application/x-json");
+		
+		String URI = request.getRequestURI();
+		if (URI.endsWith("/goals") || URI.endsWith("/goals/")) {
+			response.setHeader("Allow", "GET, POST");
+			response.sendError(405, "PUT is not supported without a specific resource.");
+			return;
+		}
+		String resource = URI.substring(URI.lastIndexOf("/") + 1);
+		if (URI.endsWith("goals/" + resource)) {
+			int id = Integer.parseInt(resource);
+			Goal result = Goal.getGoal(token.getUsername(), id);
+			if (result != null) {
+				try {
+					Goal contents = Goal.parseGoal(request.getReader(), token.getUsername());
+					if (contents != null) {
+						result.replaceContent(contents);
+						if (result.update()) {
+							response.getWriter().print(result);
+							response.setStatus(200);
+							return;
+						}
+						else {
+							response.sendError(500, "Could not save the task.");
+							return;
+						}
+					}
+					else {
+						response.sendError(400, "Contents of the resquest could not be parsed.");
+						return;
+					}
+				}
+				catch (NumberFormatException e) {
+					//TODO auto
+					response.sendError(400, "Something was wrong with the numeric parameters.");
+					e.printStackTrace();
+				}
+				return;
+			}
+			else {
+				response.sendError(404, "Goal not found, use POST on '/goals' to create.");
+				return;
+			}
+		}
+		else {
+			response.sendError(400, "Goal not properly identified.");
+			return;
+		}
 	}
 	
 }
