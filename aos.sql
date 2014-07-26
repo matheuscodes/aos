288939b88a8e6f1748883569b5c4402eb9ebd3ba
Matheus Borges Teixeira <matheus.bt@gmail.com>
2014-07-26 17:31:42 +0200

    Adding final database definition.

---
+++ aos.sql
@@ -0,0 +1,105 @@
+CREATE DATABASE  IF NOT EXISTS `aos` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `aos`;
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: aos
+-- ------------------------------------------------------
+-- Server version	5.5.37-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_name` varchar(13) NOT NULL,
+  `first_name` varchar(35) DEFAULT NULL,
+  `last_name` varchar(35) DEFAULT NULL,
+  `hashed_password` varchar(32) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `expiration_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `secret_key` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `work`
+--
+
+DROP TABLE IF EXISTS `work`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `work` (
+  `start` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `task_id` int(11) unsigned NOT NULL,
+  `result` float NOT NULL DEFAULT '0',
+  `comment` varchar(110) DEFAULT NULL,
+  `time_spent` int(11) NOT NULL,
+  PRIMARY KEY (`start`,`task_id`),
+  KEY `work_task` (`task_id`),
+  CONSTRAINT `work_task` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `goal`
+--
+
+DROP TABLE IF EXISTS `goal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `goal` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(35) CHARACTER SET latin1 NOT NULL,
+  `time_planned` float NOT NULL,
+  `description` varchar(140) DEFAULT NULL,
+  `user_name` varchar(13) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `goal_user` (`user_name`),
+  CONSTRAINT `goal_user` FOREIGN KEY (`user_name`) REFERENCES `user` (`user_name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(70) NOT NULL,
+  `target` float NOT NULL,
+  `initial` float NOT NULL DEFAULT '0',
+  `goal_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_goal` (`goal_id`),
+  CONSTRAINT `task_goal` FOREIGN KEY (`goal_id`) REFERENCES `goal` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-07-26 17:29:54
